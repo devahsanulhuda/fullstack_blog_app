8a2e51734bc7c782ec63259a98609aff14c85cda
devahsanulhuda <devahsanulhuda@gmail.com>
2024-09-01 17:54:05 +0600

    client ui + server

---
+++ admin/src/App.js
@@ -0,0 +1,37 @@
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+
+function Layout() {
+  const user = {};
+  const location = useLocation();
+
+  return user?.token ? (
+    <div className="w-full h-screen">
+      {/* <Navbar /> */}
+      <div className="w-full h-full border-t pt-16">
+        <div className="hidden lg:flex">{/* <Sidebar /> */}</div>
+        <div className="w-full flex-1 px-8 py-6 overflow-y-auto">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/auth" state={{ from: location }} replace />
+  );
+}
+
+function App() {
+  return (
+    <main className="w-full min-h-screen ">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path="/" element={<Navigate to="dashboard" />} />
+        </Route>
+
+        {/* <Route path="/auth" element={<StartPage />} />
+        <Route path="/otp-verification" element={<OTPVerification />} /> */}
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
